e297c9ffbf5022ee325bd0952f9bd0fcc469537c
jmg7173 <jmg7173@sogang.ac.kr>
2017-03-15 07:27:24 +0000

    [ERRORFIX]Syntax error fixed. Add template

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import urls
-from . import view
+from django.conf.urls import url
+from . import views
 
 
 urlpatterns = [
-    url(r'^$', view.post_list, name='post_list'),
+    url(r'^$', views.post_list, name='post_list'),
 ]
